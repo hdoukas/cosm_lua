94cae72007846a4f785b320763759442f24a701d
hdoukas <ch.doukas@gmail.com>
2012-11-08 14:45:02 +0200

    Update cosm.lua

--- cosm.lua
+++ cosm.lua
@@ -1,17 +1,17 @@
-function read_current(apikey,feed, datastream) 
+local function read_current(apikey,feed, datastream) 
 	local response = http.request {
 	method='get',
 	url = 'http://api.cosm.com/v2/feeds/'..feed..'/datastreams/'..datastream,
 	headers = 
   {
-    ["X-PachubeApiKey"] = "aeiS1ETfAjuPiu7Fzq8HIjdh4jOSAKxkYk1RNm9od2ZaTT0g"
+    ["X-PachubeApiKey"] = tostring(apikey)
   }
 }
 
 	return json.parse(response.content).current_value
 end
 
-function read_24h(apikey, feed, datastream) 
+local function read_24h(apikey, feed, datastream) 
 	local response = http.request {
 	method='get',
 	url = 'http://api.cosm.com/v2/feeds/'..feed..'/datastreams/'..datastream..'/?duration=24hours&interval=60',
@@ -27,11 +27,11 @@ function read_24h(apikey, feed, datastream)
 	local max = -math.huge
 	local min = math.huge
 	for i=1,# values do
-      sum=sum+values[i].value
-			max = math.max( max, values[i].value)
-		  min = math.min( min, values[i].value)
-  end
+        	sum=sum+values[i].value
+        	max = math.max( max, values[i].value)
+		min = math.min( min, values[i].value)
+  	end
 	return {sum/#values, max, min}
-	--return debug
+
 		
 end
